ba97ddcc429a1ecf1a6f3a3ad95d5b2138e27519
krishi-jain21 <krishijain212121@gmail.com>
2024-05-06 17:04:08 +0530

    Assignment 2

--- src/com/pengwin/krishi/jdbc/day1/Main.java
+++ src/com/pengwin/krishi/jdbc/day1/Main.java
@@ -2,15 +2,21 @@ package com.pengwin.krishi.jdbc.day1;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		RecordDisplay r1 = new RecordDisplay("northwind", "root", "root");
-		r1.connect();
-		
-		r1.displayRecords();
-		
-		r1.disconnect();
-	}
+    public static void main(String[] args) {
+        // Create an instance of RecordDisplay with database credentials
+        RecordDisplay r1 = new RecordDisplay("northwind", "root", "root");
+        
+        // Connect to the database
+        r1.connect();
+        
+        // Display all records
+        r1.displayRecords(null);
+        
+        // Display records for employee with ID 1
+        r1.displayRecords(1);
+        
+        // Disconnect from the database
+        r1.disconnect();
+    }
 
 }
--- src/com/pengwin/krishi/jdbc/day1/RecordDisplay.java
+++ src/com/pengwin/krishi/jdbc/day1/RecordDisplay.java
@@ -1,28 +1,40 @@
 /**
- * 
+ * RecordDisplay class provides methods to connect to a MySQL database,
+ * display employee records, and disconnect from the database.
  */
 package com.pengwin.krishi.jdbc.day1;
 
 import java.sql.*;
+
 /**
- * 
+ * RecordDisplay class provides methods to connect to a MySQL database,
+ * display employee records, and disconnect from the database.
  */
 public class RecordDisplay {
 
-	private Connection connection;
-	private Statement statement;
-	private String dbUrl = "jdbc:mysql://localhost:3306/";
+    private Connection connection;
+    private Statement statement;
+    private String dbUrl = "jdbc:mysql://localhost:3306/";
     private String dbUserName;
     private String dbPassword;
-    
-    
-    public RecordDisplay(String dbName,String dbUserName, String dbPassword){
-    	
-    	this.dbUserName = dbUserName;
-    	this.dbPassword = dbPassword;
-    	this.dbUrl += dbName;
-    	
+
+    /**
+     * Constructor to initialize the RecordDisplay object with database name,
+     * username, and password.
+     * 
+     * @param dbName     The name of the database to connect to.
+     * @param dbUserName The username for database authentication.
+     * @param dbPassword The password for database authentication.
+     */
+    public RecordDisplay(String dbName, String dbUserName, String dbPassword) {
+        this.dbUserName = dbUserName;
+        this.dbPassword = dbPassword;
+        this.dbUrl += dbName;
     }
+
+    /**
+     * Connects to the database using the provided credentials.
+     */
     public void connect() {
         try {
             // Register JDBC driver
@@ -36,48 +48,58 @@ public class RecordDisplay {
             e.printStackTrace();
         }
     }
-    
-    
-    
-    public void displayRecords() {
-        String query = "SELECT employeeid AS EmployeeID, CONCAT(first_name, ' ', last_name) AS Name, title AS Title, CONCAT(address, ', ', city, ', ', country) AS Address, homephone AS HomePhone FROM employee";
-        try {
-            ResultSet resultSet = statement.executeQuery(query);
 
+    /**
+     * Displays employee records from the database. If empId is provided,
+     * displays records for the specified employee.
+     * 
+     * @param empId The ID of the employee to display records for. If null, all
+     *              records will be displayed.
+     */
+    public void displayRecords(Integer empId) {
+        String query = "SELECT employeeid AS EmployeeID, CONCAT(first_name, ' ', last_name) AS Name, title AS Title, CONCAT(address, ', ', city, ', ', country) AS Address,"
+        		+ " homephone AS HomePhone FROM employee";
+        if (empId != null) {
+            query += " WHERE employeeid = ?";
+        }
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            if (empId != null) {
+                statement.setInt(1, empId);
+            }
+            ResultSet resultSet = statement.executeQuery();
             System.out.println("EmployeeID  |  Name                    |  Title        |  Address                                     |  HomePhone");
             System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
-
-            while(resultSet.next()) {
+            while (resultSet.next()) {
                 int employeeId = resultSet.getInt("EmployeeID");
                 String name = resultSet.getString("Name");
                 String title = resultSet.getString("Title");
                 String address = resultSet.getString("Address");
                 String homePhone = resultSet.getString("HomePhone");
-                
+
                 // Adjust spacing as needed based on your actual data lengths
-                System.out.printf("%-11d|  %-24s|  %-12s|  %-45s|  %-13s%n", employeeId, name, title, address, homePhone);
+                System.out.printf("%-11d|  %-24s|  %-12s|  %-45s|  %-13s%n", employeeId, name, title, address,homePhone);
             }
             resultSet.close();
-        } catch(SQLException e) {
+            statement.close(); // Close the statement
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
+
+    /**
+     * Disconnects from the database.
+     */
     public void disconnect() {
-    	try {
-    		if (connection!= null) {
-    			connection.close();
-    			System.out.println("Disconnected from the database"); 
-    		}
-    		
-    	}
-    	catch( SQLException e) {
-    		e.printStackTrace();
-    	}
-    	
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Disconnected from the database");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
+
 }
-
-
-
